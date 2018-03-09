8a916f43a8926d695c3a0b6bf397ec89d1b6f2d2
Evaline Bai <evalinebai@gmail.com>
2018-03-08 21:22:47 -0800

    Update walmartHelpers.js

--- helpers/walmartHelpers.js
+++ helpers/walmartHelpers.js
@@ -2,10 +2,6 @@ const request = require('request');
 const axios = require('axios');
 var Promise = require('bluebird');
 
-// var terms = ['apple juice', 'butter', 'bacon']
-// var term = 'peanut butter';
-// var walmartLabsKey = '5jggjhsnfbfww54agx8f7vb8';
-
 const getProducts = function (terms, cb) {
   var promises = [];
   for (var i = 0; i < terms.length; i++ ) {
@@ -17,5 +13,5 @@ const getProducts = function (terms, cb) {
 }
 
 const searchOneTerm = function (term) {
-    return axios.get(`http://api.walmartlabs.com/v1/search?apiKey=${walmartLabsKey}&query=${term}&sort=relevance`)
+    return axios.get(`http://api.walmartlabs.com/v1/search?apiKey=${process.env.WALMART_KEY}&query=${term}&sort=relevance`)
 }
