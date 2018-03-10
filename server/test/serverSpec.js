3770c4cd100ce516b345e58756ee509475076792
Michael Butak <mikebutak@gmail.com>
2018-03-10 14:38:08 -0600

    setup for mocha chai testing

---
+++ server/test/serverSpec.js
@@ -0,0 +1,26 @@
+const expect = require('chai').expect;
+const assert = require('chai').assert;
+const server = require('../index.js');
+const path = require('path');
+const supertest = require('supertest');
+
+var request = supertest.agent(server);
+
+// describe('server', function() {
+//   describe('GET /', function () {
+//     it('should return the content of index.html', function (done) {
+//       // just assume that if it contains a <div> tag its index.html
+//       request
+//         .get('/')
+//         .expect(200, /<div/, done);
+//     });
+//   });
+// });
+
+
+describe('Server', function () {
+  it('/signup should add a new user to the database', function () {
+    let result = serve.METHODNAMEGOESHERE
+    assert.equal(result, 'put the expected result here')
+  })
+})
